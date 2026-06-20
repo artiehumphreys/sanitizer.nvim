ada9bf66a067a93444fa6ef63b1b2a46433a9e11
artiehumphreys <artieh2003@gmail.com>
2026-06-20 14:43:29 -0400

    feat(cmd): nested tab completion

--- plugin/sanitizer.lua
+++ plugin/sanitizer.lua
@@ -3,16 +3,38 @@ if vim.g.loaded_sanitizer then
 end
 vim.g.loaded_sanitizer = true
 
+local sanitizers = { "address", "thread", "undefined", "memory", "leak" }
+
+local arg_completions = {
+  build = { sanitizers },
+  run = { sanitizers },
+  clean = { sanitizers },
+  results = {},
+  stop = {},
+}
+
 vim.api.nvim_create_user_command("San", function(opts)
   require("sanitizer").run_command(opts.fargs)
 end, {
   nargs = "+",
-  complete = function(subcommand_arg_lead)
-    local san_args = { "address", "leak", "memory", "thread", "undefined" }
+  complete = function(arg_lead, cmd_line, _)
+    local words = vim.split(vim.trim(cmd_line), "%s+")
+    local typing_new = cmd_line:sub(-1) == " "
+
+    local argc = #words - 1 + (typing_new and 1 or 0)
+
+    local candidates
+    if argc == 1 then
+      candidates = vim.tbl_keys(arg_completions)
+    else
+      local subcommand = words[2]
+      local spec = arg_completions[subcommand]
+      candidates = spec and spec[argc - 1] or {}
+    end
     return vim
-      .iter(san_args)
-      :filter(function(arg)
-        return arg:find(subcommand_arg_lead) ~= nil
+      .iter(candidates)
+      :filter(function(c)
+        return vim.startswith(c, arg_lead)
       end)
       :totable()
   end,
